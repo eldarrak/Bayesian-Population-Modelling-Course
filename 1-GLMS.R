cbcae87466be9c4d019581ca360635485136ade3
Eldar laptop <eldarrak@gmail.com>
2018-11-27 09:11:36 +0100

    upd

--- 1-GLMS.R
+++ 1-GLMS.R
@@ -1,2 +1,8 @@
 # 1. Linear models and Poisson GLM in R and JAGS
 ## required data falcons.txt
+
+y <- c(25, 14, 68, 79, 64, 139, 49, 119, 111)
+A <- factor(c(1, 1, 1, 2, 2, 2, 3, 3, 3))
+X <- c(1, 14, 22, 2, 9, 20, 2, 13, 22)
+plot(X, y, col = c(rep("red", 3), rep("blue", 3), rep("green", 3)), xlim = c(-1, 25), ylim = c(0, 140))
+
