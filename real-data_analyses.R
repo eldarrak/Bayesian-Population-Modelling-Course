ea7705a32cc81b7faee2e0abf552131c2a3fff0f
Eldar laptop <eldarrak@gmail.com>
2018-11-30 11:03:40 +0100

    upd

--- real-data_analyses.R
+++ real-data_analyses.R
@@ -1,6 +1,6 @@
 
 
-Dat<-read.csv('File_name_csv')
+Dat<-read.csv('Great_tit_AdultsOnly.csv')
 
 
 str(Dat)
@@ -12,4 +12,12 @@ head(Tab)
 
 Tab<-apply(Tab, c(1,2), sign)
 
+# remove last year first marking
+
+remove.last<-function(CH) {
+  Index<-apply(CH, 1, FUN=function(x) ifelse(which(x==1)[1] == length(x), FALSE, TRUE))
+  return(CH[Index,])
+}
+
+Tab<-remove.last(Tab)
 
